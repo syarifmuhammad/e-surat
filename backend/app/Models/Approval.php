adb22b96bc604e450b4c2f4a03b161491ae21ade
syarifmuhammad <syarifmuhammad369@gmail.com>
2024-01-22 20:28:47 +0700

    Add methods to create and update approvals and signers for a letter

--- backend/app/Models/Approval.php
+++ backend/app/Models/Approval.php
@@ -15,7 +15,58 @@ class Approval extends Model
         'is_signer',
     ];
 
-    public function employee() {
+    public function employee()
+    {
         return $this->belongsTo(Employee::class, 'employee_id', 'id');
+    }
+
+    public static function create_approvals_and_signers_to_letter($letter, $signers, $approvals)
+    {
+        $tmp_signers = [];
+        foreach ($signers as $signer) {
+            $tmp = new Approval();
+            $tmp->employee_id = $signer['employee']['id'];
+            $tmp->position = $signer['position'];
+            $tmp->is_signer = true;
+            $tmp_signers[] = $tmp->toArray();
+        }
+
+        $letter->approvals()->createMany($tmp_signers);
+
+        $tmp_approvals = [];
+        foreach ($approvals as $approval) {
+            $tmp = new Approval();
+            $tmp->employee_id = $approval['employee']['id'];
+            $tmp->position = $approval['position'];
+            $tmp_approvals[] = $tmp->toArray();
+        }
+
+        $letter->approvals()->createMany($tmp_approvals);
+    }
+
+    public static function update_approvals_and_signers_to_letter($letter, $signers, $approvals)
+    {
+        $letter->signers()->delete();
+        $tmp_signers = [];
+        foreach ($signers as $signer) {
+            $tmp = new Approval();
+            $tmp->employee_id = $signer['employee']['id'];
+            $tmp->position = $signer['position'];
+            $tmp->is_signer = true;
+            $tmp_signers[] = $tmp->toArray();
+        }
+
+        $letter->approvals()->createMany($tmp_signers);
+
+        $letter->approvals()->delete();
+        $tmp_approvals = [];
+        foreach ($approvals as $approval) {
+            $tmp = new Approval();
+            $tmp->employee_id = $approval['employee']['id'];
+            $tmp->position = $approval['position'];
+            $tmp_approvals[] = $tmp->toArray();
+        }
+
+        $letter->approvals()->createMany($tmp_approvals);
     }
 }
