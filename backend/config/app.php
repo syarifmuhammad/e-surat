2e9204e988892d6aae9fcaccf779a7e82a040176
syarifmuhammad <syarifmuhammad369@gmail.com>
2024-01-22 20:27:16 +0700

    Add TimeIntervalConverterServiceProvider to app providers

--- backend/config/app.php
+++ backend/config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TerbilangServiceProvider::class,
+        App\Providers\TimeIntervalConverterServiceProvider::class,
     ])->toArray(),
 
     /*
